b78a8b5984717dd1ab03163550abba28c115b69b
qianlifeng <qianlf2010@gmail.com>
2024-06-05 23:51:19 +0800

    Update test script and enable sourceMap in tsconfig
    The test script in `src/__tests__/test.ts` has been updated for better functionality and improved plugin initialization. The `sourceMap` property in the `tsconfig.json` file has been set to `true` to generate corresponding '.map' files. The `Runtime` in `

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,3 +1,28 @@
-test("4 greater than 1", async () => {
-  expect(4).toBeGreaterThan(1);
+import { Context, PublicAPI, Query } from "@wox-launcher/wox-plugin"
+import { plugin } from "../index"
+
+test("query", async () => {
+  const ctx = {} as Context
+  const query = {
+    Env: { "ActiveWindowTitle": "" },
+    RawQuery: "wpm install ",
+    Selection: { Type: "text", Text: "", FilePaths: [] },
+    Type: "input",
+    Search: "",
+    TriggerKeyword: "wpm",
+    Command: "install",
+    IsGlobalQuery(): boolean {
+      return false
+    }
+  } as Query
+
+  await plugin.init(ctx, {
+    PluginDirectory: "", API: {
+      Log: (ctx, level, message) => {
+        console.log(level, message)
+      }
+    } as PublicAPI
+  })
+  const results = await plugin.query(ctx, query)
+  expect(results.length).toBeGreaterThan(0)
 })
